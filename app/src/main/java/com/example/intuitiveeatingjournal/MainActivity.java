5fcb8aa85da6bf8c11adfc76a3d5e8fcc7fd82c4
Amanda Vu <amanda321@berkeley.edu>
2020-09-11 12:48:54 -0400

    tab layout working!!

---
+++ app/src/main/java/com/example/intuitiveeatingjournal/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.intuitiveeatingjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Tab Layout
+        TabLayout tabLayout = findViewById(R.id.tabBar);
+        TabItem tabToday = findViewById(R.id.tabToday);
+        TabItem tabStats = findViewById(R.id.tabStats);
+        TabItem tabHistory = findViewById(R.id.tabHistory);
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),
+                tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
