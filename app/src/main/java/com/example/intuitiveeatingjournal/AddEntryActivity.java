6f0e63df7c3c2a15865f0c5274d60ec88d793174
Amanda Vu <amanda321@berkeley.edu>
2020-09-12 13:34:09 -0400

    form submit takes us back to main view

--- app/src/main/java/com/example/intuitiveeatingjournal/AddEntryActivity.java
+++ app/src/main/java/com/example/intuitiveeatingjournal/AddEntryActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class AddEntryActivity extends AppCompatActivity {
 
     @Override
@@ -18,10 +20,15 @@ public class AddEntryActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
+        Snackbar.make(view, "bllop a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+        // Get the form objects
         EditText editText = (EditText) findViewById(R.id.foodEntry);
         SeekBar seekbar1 = (SeekBar) findViewById(R.id.hungerBefore);
         SeekBar seekbar2 = (SeekBar) findViewById(R.id.hungerAfter);
 
+        // Extract form information
         String entry = editText.getText().toString();
         int before = seekbar1.getProgress();
         int after = seekbar2.getProgress();
@@ -31,9 +38,8 @@ public class AddEntryActivity extends AppCompatActivity {
         bundle.putInt("before", before);
         bundle.putInt("after", after);
 
-        FragmentToday fragmentToday = (FragmentToday)
-                getSupportFragmentManager().findFragmentById(R.id.fragmentToday);
 
-        fragmentToday.updateList(bundle);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/intuitiveeatingjournal/FragmentToday.java
+++ app/src/main/java/com/example/intuitiveeatingjournal/FragmentToday.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.FileUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class FragmentToday extends Fragment {
@@ -22,24 +28,45 @@ public class FragmentToday extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_today, container, false);
 
+        // Button to add an entry
         FloatingActionButton addButton = rootView.findViewById(R.id.buttonAddEntry);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 Intent intent = new Intent(getActivity(), AddEntryActivity.class);
                 startActivity(intent);
             }
         });
+
+        // List View
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+
         return rootView;
     }
 
-    public void updateList(Bundle args) {
-        String entry = args.getString("entry");
-        Integer before = args.getInt("before");
-        Integer after = args.getInt("after");
-        Snackbar.make(this.getView(), entry, Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
