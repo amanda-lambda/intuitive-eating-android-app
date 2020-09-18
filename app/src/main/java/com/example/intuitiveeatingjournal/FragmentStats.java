37279e70dbcca2a09a40d69a16a1dd3f639e2771
Amanda Vu <amanda321@berkeley.edu>
2020-09-18 09:04:34 -0400

    interleaving working!

--- app/src/main/java/com/example/intuitiveeatingjournal/FragmentStats.java
+++ app/src/main/java/com/example/intuitiveeatingjournal/FragmentStats.java
@@ -1,5 +1,6 @@
 package com.example.intuitiveeatingjournal;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,10 +13,10 @@ import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.ScrollView;
 
+import java.util.ArrayList;
+
 
 public class FragmentStats extends Fragment {
-
-    private ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,10 +26,55 @@ public class FragmentStats extends Fragment {
 
         // Interleave
         MainActivity activity = (MainActivity) getActivity();
+        ArrayList<String> cellValues = new ArrayList<String>();
+        ArrayList<Integer> cellColors = new ArrayList<Integer>();
+
+        int numColumns = 13;
+        int middleColumn = (numColumns - 1) / 2;
+        int numEntries = activity.befores.size();
+        int numRows = (numEntries + numColumns - 1) / numColumns;
+
+        int count = 0;
+        for (int j = 0; j < numRows; j++) {
+            // Before half
+            for (int i = 0; i < middleColumn; i++) {
+                if (count >= numEntries) {
+                    continue;
+                }
+                String before = activity.befores.get(count);
+                Integer before_pos = new Integer(before);
+                Integer before_color = activity.colors.get(before_pos);
+                cellValues.add(before);
+                cellColors.add(before_color);
+                count = count + 1;
+            }
+            // Empty
+            cellValues.add("");
+            cellColors.add(Color.WHITE);
+
+            // After half
+            for (int i = 0; i < middleColumn; i++) {
+                if (count >= numEntries) {
+                    continue;
+                }
+                String after = activity.afters.get(count);
+                Integer after_pos = new Integer(after);
+                Integer after_color = activity.colors.get(after_pos);
+                cellValues.add(after);
+                cellColors.add(after_color);
+                count = count + 1;
+            }
+        }
 
         // Grid View
         GridView gridView = (GridView) rootView.findViewById(R.id.gridView);
-        gridView.setAdapter(new ArrayAdapter<String>(activity, R.layout.cell, activity.befores));
+        gridView.setAdapter(new ArrayAdapter<String>(activity, R.layout.cell, cellValues));
+
+        // Set color
+//        int count = gridView.getCount();
+//        for(int i=0;i<count;i++){
+//            gridView.getChildAt(i).setBackgroundColor(Color.parseColor("#18A608"));
+//        }
         return rootView;
     }
 }
